06c6a263666fa15b12e2dd4f2d8ea17a153b9dd7
LiWeny16 <a454888395@gmail.com>
2023-04-07 15:57:46 +0800

    update

--- script.js
+++ script.js
@@ -87,7 +87,7 @@ const handleSubmit = async (e) => {
     // http://47.113.229.110:8082
     // http://127.0.0.1:5500/client/#
     try {
-      const response = await fetch('https://6ba3ea08.r2.cpolar.top/', {
+      const response = await fetch('https://api.bigonion.cn/', {
         method: "POST",
         headers: {
           'Content-Type': "application/json"

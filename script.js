3aebf18724a30dfc6487e574279eeb13bc0c0a4f
LiWeny16 <a454888395@gmail.com>
2023-06-09 21:16:25 +0800

    update

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function typeText(element, text) {
         element.innerHTML += text[index]
         element.innerHTML = element.innerHTML.replace(/  \n/g, "<br />")
         if (element.innerHTML.match(/```/g) ? (element.innerHTML.match(/```/g).length % 2 === 0) : false) {
-          element.innerHTML = marked.parse(text.substring(0,index+1))
+          element.innerHTML = marked.parse(text.substring(0, index + 1))
           hljs.highlightAll()
         }
         goToFooter(document.getElementById("chat_container"))
@@ -44,6 +44,7 @@ function typeText(element, text) {
         // let mdText = marked.parse(text.replace(/\n /g, "<br />"))
         element.innerHTML = marked.parse(text)
         hljs.highlightAll()
+        goToFooter(document.getElementById("chat_container"))
         index++
       }
       else {
@@ -147,3 +148,54 @@ function goToFooter(ele) {
   var div = ele;
   div.scrollTop = div.scrollHeight;
 }
+
+/**
+ * 相似度对比
+ * @description 作者：三苗同学
+                链接：https://juejin.cn/post/7086353349544509447
+ * @param s 文本1
+ * @param t 文本2
+ * @param f 小数位精确度，默认2位
+ * @returns {string|number|*} 百分数前的数值，最大100. 比如 ：90.32
+ */
+function similar(s, t, f) {
+  if (!s || !t) {
+    return 0
+  }
+  if (s === t) {
+    return 100;
+  }
+  var l = s.length > t.length ? s.length : t.length
+  var n = s.length
+  var m = t.length
+  var d = []
+  f = f || 2
+  var min = function (a, b, c) {
+    return a < b ? (a < c ? a : c) : (b < c ? b : c)
+  }
+  var i, j, si, tj, cost
+  if (n === 0) return m
+  if (m === 0) return n
+  for (i = 0; i <= n; i++) {
+    d[i] = []
+    d[i][0] = i
+  }
+  for (j = 0; j <= m; j++) {
+    d[0][j] = j
+  }
+  for (i = 1; i <= n; i++) {
+    si = s.charAt(i - 1)
+    for (j = 1; j <= m; j++) {
+      tj = t.charAt(j - 1)
+      if (si === tj) {
+        cost = 0
+      } else {
+        cost = 1
+      }
+      d[i][j] = min(d[i - 1][j] + 1, d[i][j - 1] + 1, d[i - 1][j - 1] + cost)
+    }
+  }
+  let res = (1 - d[n][m] / l) * 100
+  return res.toFixed(f)
+}
+
